d965e2ca5cbd921533e8b0e6a809903d5eddb91e
Arman Nikoyan <armannikoyan04@icloud.com>
2024-07-29 20:45:00 +0400

    Start

---
+++ Makefile
@@ -0,0 +1,64 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/07/29 18:10:52 by anikoyan          #+#    #+#              #
+#    Updated: 2024/07/29 20:36:31 by anikoyan         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = minishell
+
+SRC_DIR = src
+UTILS_DIR = utils
+OBJ_DIR = obj
+LIBFT_DIR = libft
+
+SRC_FILES = main.c
+UTILS_FILES = formatters.c
+
+SRC = $(addprefix $(SRC_DIR)/, $(SRC_FILES))
+UTILS = $(addprefix $(SRC_DIR)/$(UTILS_DIR)/, $(UTILS_FILES))
+ALL_SRC = $(SRC) $(UTILS)
+
+OBJ = $(addprefix $(OBJ_DIR)/, $(patsubst %.c, %.o, $(notdir $(ALL_SRC))))
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+INC = -I $(LIBFT_DIR)
+
+LIBFT = $(LIBFT_DIR)/libft.a
+
+RM = rm -f
+
+all: $(OBJ_DIR) $(LIBFT) $(NAME)
+
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	$(CC) $(CFLAGS) $(INC) -c $< -o $@
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/$(UTILS_DIR)/%.c
+	$(CC) $(CFLAGS) $(INC) -c $< -o $@
+
+$(NAME): $(OBJ)
+	$(CC) $(OBJ) $(LIBFT) -o $(NAME)
+
+$(LIBFT):
+	make -C $(LIBFT_DIR)
+
+clean:
+	$(RM) -r $(OBJ_DIR)
+	make -C $(LIBFT_DIR) clean
+
+fclean: clean
+	$(RM) $(NAME)
+	make -C $(LIBFT_DIR) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ src/utils/formatters.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   formatters.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 20:36:38 by anikoyan          #+#    #+#             */
+/*   Updated: 2024/07/29 20:43:59 by anikoyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+void	ft_mtx_free(char **mtx)
+{
+	int	i;
+
+	i = 0;
+	while (mtx[i])
+	{
+		free(mtx[i]);
+		i++;
+	}
+	free(mtx);
+}
+
+char	**ft_format_argv(int argc, char **argv)
+{
+	char	**data;
+	int		i;
+
+	if (argc < 5)
+		return (NULL);
+	data = malloc(sizeof(char *) * argc);
+	i = 0;
+	while (argv[i + 1])
+	{
+		data[i] = ft_strdup(argv[i + 1]);
+		if (!data[i])
+		{
+			ft_mtx_free(data);
+			return (NULL);
+		}
+		i++;
+	}
+	data[i] = NULL;
+	return (data);
+}
+
+char	**ft_get_envp(char **envp)
+{
+	int		i;
+	char	**path;
+
+	i = 0;
+	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5))
+		i++;
+	if (!envp[i])
+		return (NULL);
+	path = ft_split(envp[i] + 5, ':');
+	return (path);
+}
+
+char	*ft_command_path(char *cmd, char **envp)
+{
+	char	*line;
+	char	*tmp;
+	int		i;
+
+	if (access(cmd, X_OK) == 0)
+		return (cmd);
+	i = 0;
+	while (envp[i])
+	{
+		tmp = ft_strjoin("/", cmd);
+		if (!tmp)
+			return (NULL);
+		line = ft_strjoin(envp[i], tmp);
+		if (!line)
+			return (NULL);
+		free(tmp);
+		if (access(line, X_OK) == 0)
+			return (line);
+		free(line);
+		i++;
+	}
+	return (NULL);
+}
