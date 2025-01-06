43c5b2cb4deeb33e399c80c7376581f3616bbcab
Arman Nikoyan <armannikoyan04@icloud.com>
2025-01-06 18:21:49 +0400

    removed: unnecessary line that causes double free

--- src/utils/ft_tokendelone.c
+++ src/utils/ft_tokendelone.c
@@ -6,7 +6,7 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 12:25:11 by anikoyan          #+#    #+#             */
-/*   Updated: 2024/12/30 21:21:53 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/01/06 18:20:52 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 
 void	ft_tokendelone(void *token)
 {
-	// free(((t_token *)token)->content);
 	free((t_token *)token);
 }
