15fcfef758c1fef3e233cda5f61a78230f00acd8
Arman Nikoyan <armannikoyan04@icloud.com>
2025-01-11 05:21:56 -0500

    fixed: quote removal works as expected

--- src/tokenization/ft_argument_quote.c
+++ src/tokenization/ft_argument_quote.c
@@ -6,59 +6,66 @@
 /*   By: gsimonia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 16:22:52 by gsimonia          #+#    #+#             */
-/*   Updated: 2025/01/10 15:57:28 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/01/11 05:20:11 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-// ISSUE: cases: echo "123"'4'5 -> 12345 and echo "123'4'5 -> syntax error arent working
-
-static int	is_starting_quote(char c)
-{
-	return (c == '\'' || c == '\"');
-}
-
-static char	*process_quote_remove(const char *content, char starting_quote)
-{
-	size_t	i;
-	size_t	j;
-	char	*result;
-
-	result = malloc(ft_strlen(content) + 1);
-	if (!result)
-	{
-		perror("Memory allocation failed");
-		exit(EXIT_FAILURE);
-	}
-	i = 1;
-	j = 0;
-	while (content[i])
-	{
-		if (content[i] == starting_quote)
-		{
-			i++;
-			continue ;
-		}
-		result[j++] = content[i++];
-	}
-	result[j] = '\0';
-	return (result);
-}
-
 void	ft_quote_removal(t_token *token)
 {
-	char	starting_quote;
-	char	*processed_content;
+	char	quote;
+	char	*new_content;
+	unsigned short	len;
+	unsigned short	i;
+	unsigned short	j;
 
 	if (!token || !token->content)
 		return ;
-	starting_quote = token->content[0];
-	if (!is_starting_quote(starting_quote))
+	len = 0;
+	i = 0;
+	while (token->content[i])
+	{
+		while (token->content[i] && token->content[i] != '\'' && token->content[i] != '\"')
+		{
+			len++;
+			i++;
+		}
+		if (token->content[i])
+		{
+			quote = token->content[i];
+			i++;
+		}
+		while (token->content[i] && token->content[i] != quote)
+		{
+			len++;
+			i++;
+		}
+		if (token->content[i])
+			i++;
+	}
+	if (len && len == ft_strlen(token->content))
 		return ;
-	processed_content = process_quote_remove(token->content, starting_quote);
+	new_content = (char *)malloc(sizeof(char) * (len + 1));
+	i = 0;
+	j = 0;
+	while (token->content[i])
+	{
+		while (token->content[i] && token->content[i] != '\'' && token->content[i] != '\"')
+			new_content[j++] = token->content[i++];
+		if (token->content[i])
+		{
+			quote = token->content[i];
+			i++;
+		}
+		while (token->content[i] && token->content[i] != quote)
+			new_content[j++] = token->content[i++];
+		if (token->content[i])
+			i++;
+	}
+	new_content[j] = '\0';
 	free(token->content);
-	token->content = processed_content;
+	token->content = new_content;
 }
 
 bool	ft_is_pattern_match(t_token *token)
