abacb18d2762be65332c7edf726b81446b14b8e2
Gaik Simonian <gaiksimonian@MacBook-Pro-GS.local>
2025-01-14 18:33:53 +0400

    env_exp norm fix

--- src/parsing/ft_env_expansion.c
+++ src/parsing/ft_env_expansion.c
@@ -6,7 +6,7 @@
 /*   By: gsimonia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 14:49:16 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/01/14 15:58:02 by gsimonia         ###   ########.fr       */
+/*   Updated: 2025/01/14 18:12:09 by gsimonia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,107 +49,142 @@ char	*ft_get_env(char *str, char **envp)
 	return ("");
 }
 
-static unsigned short calculate_env_length(char *input, char **envp)
+static unsigned short	calculate_non_env_length(char *input,
+		unsigned short *i, bool *opened_quote)
 {
-    unsigned short len;
-    unsigned short i;
-    bool opened_quote;
+	unsigned short	len;
 
-    len = 0;
-    i = 0;
-    opened_quote = false;
-
-    while (input[i])
-    {
-        while (input[i] && (input[i] != '$' || (input[i] == '$' && opened_quote)))
-        {
-            if (input[i] == '\'')
-                opened_quote = !opened_quote;
-            len++;
-            i++;
-        }
-        if (input[i] && ((input[i + 1] && !ft_isalnum(input[i + 1]))
-            || !input[i + 1]))
-        {
-            i++;
-            len++;
-        }
-        else if (input[i])
-        {
-            i++;
-            if (input[i] && input[i] != '?')
-            {
-                len += ft_strlen(ft_get_env(input + i, envp));
-                len -= ft_get_env_name_len(input + i);
-            }
-            else if (input[i])
-                len++;
-        }
-    }
-    return (len);
+	len = 0;
+	while (input[*i]
+		&& (input[*i] != '$' || (input[*i] == '$' && *opened_quote)))
+	{
+		if (input[*i] == '\'')
+			*opened_quote = !*opened_quote;
+		len++;
+		(*i)++;
+	}
+	if (input[*i]
+		&& ((input[*i + 1] && !ft_isalnum(input[*i + 1])) || !input[*i + 1]))
+	{
+		(*i)++;
+		len++;
+	}
+	return (len);
 }
 
-void ft_process_quotes(char *input, unsigned short *i, char *output, unsigned short *j)
+static unsigned short	calculate_env_variable_length(char *input,
+		unsigned short *i, char **envp)
 {
-    bool opened_quote = false;
+	unsigned short	len;
 
-    while (input[*i] && (input[*i] != '$' || (input[*i] == '$' && opened_quote)))
-    {
-        if (input[*i] == '\'')
-            opened_quote = !opened_quote;
-        output[(*j)++] = input[(*i)++];
-    }
-    return ;
+	len = 0;
+	if (input[*i])
+	{
+		(*i)++;
+		if (input[*i] && input[*i] != '?')
+		{
+			len += ft_strlen(ft_get_env(input + *i, envp));
+			len -= ft_get_env_name_len(input + *i);
+		}
+		else if (input[*i])
+			len++;
+	}
+	return (len);
 }
 
-char *ft_handle_dollar_sign(char *input, char *output, unsigned short *i, unsigned short *j)
+static unsigned short	calculate_env_length(char *input,
+		char **envp)
 {
-    output[*j] = input[*i];
-    (*i)++;
-    (*j)++;
-    return (output);
+	unsigned short		len;
+	unsigned short		i;
+	bool				opened_quote;
+
+	len = 0;
+	i = 0;
+	opened_quote = false;
+	while (input[i])
+	{
+		len += calculate_non_env_length(input, &i, &opened_quote);
+		if (input[i])
+			len += calculate_env_variable_length(input, &i, envp);
+	}
+	return (len);
 }
 
-char *ft_finalize_output(char *output)
+void	ft_process_quotes(char *input,
+		unsigned short *i, char *output, unsigned short *j)
 {
-    char *tmp = output;
-    output = ft_space_correction(output);
-    free(tmp);
-    return (output);
+	bool	opened_quote;
+
+	opened_quote = false;
+	while (input[*i]
+		&& (input[*i] != '$' || (input[*i] == '$' && opened_quote)))
+	{
+		if (input[*i] == '\'')
+			opened_quote = !opened_quote;
+		output[(*j)++] = input[(*i)++];
+	}
+	return ;
 }
 
-char *ft_env_expansion(char *input, char **envp)
+char	*ft_handle_dollar_sign(char *input,
+		char *output, unsigned short *i, unsigned short *j)
 {
-    char *output;
-    char *tmp;
-    unsigned short i;
-    unsigned short j;
+	output[*j] = input[*i];
+	(*i)++;
+	(*j)++;
+	return (output);
+}
 
-    i = calculate_env_length(input, envp);
-    output = (char *)malloc(sizeof(char) * (i + 1));
-    if (!output)
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (input[i])
-    {
-        ft_process_quotes(input, &i, output, &j);
-        if (input[i] && ((input[i + 1] && !ft_isalnum(input[i + 1])) || !input[i + 1]))
-            output = ft_handle_dollar_sign(input, output, &i, &j);
-        else if (input[i])
-        {
-            i++;
-            if (input[i] && input[i] != '?')
-            {
-                tmp = ft_get_env(input + i, envp);
-                ft_strcpy(output + j, tmp);
-                j += ft_strlen(tmp);
-                i += ft_get_env_name_len(input + i);
-            }
-            else if (input[i])
-                output[j++] = input[i++];
-        }
-    }
-    output[j] = '\0';
-    return (ft_finalize_output(output));
+char	*ft_finalize_output(char *output)
+{
+	char	*tmp;
+
+	tmp = output;
+	output = ft_space_correction(output);
+	free(tmp);
+	return (output);
+}
+
+void	process_input(char *input, char **envp, char *output, unsigned short *i)
+{
+	unsigned short	j;
+	char			*tmp;
+
+	j = 0;
+	while (input[*i])
+	{
+		ft_process_quotes(input, i, output, &j);
+		if (input[*i] && ((input[*i + 1]
+					&& !ft_isalnum(input[*i + 1])) || !input[*i + 1]))
+			output = ft_handle_dollar_sign(input, output, i, &j);
+		else if (input[*i])
+		{
+			(*i)++;
+			if (input[*i] && input[*i] != '?')
+			{
+				tmp = ft_get_env(input + *i, envp);
+				ft_strcpy(output + j, tmp);
+				j += ft_strlen(tmp);
+				*i += ft_get_env_name_len(input + *i);
+			}
+			else if (input[*i])
+				output[j++] = input[(*i)++];
+		}
+	}
+	output[j] = '\0';
+}
+
+char	*ft_env_expansion(char *input, char **envp)
+{
+	char			*output;
+	unsigned short	i;
+
+	i = calculate_env_length(input, envp);
+	output = (char *)malloc(sizeof(char) * (i + 1));
+	if (!output)
+		return (NULL);
+	i = 0;
+	process_input(input, envp, output, &i);
+	return (ft_finalize_output(output));
 }
