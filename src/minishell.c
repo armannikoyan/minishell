0ff0b7d4543c59b8338e2eebcc94a993c1dc7e9b
Arman Nikoyan <armannikoyan04@icloud.com>
2025-03-13 11:37:09 +0400

    added: signal handling, term config and user input reading

---
+++ src/minishell.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 20:17:13 by anikoyan          #+#    #+#             */
+/*   Updated: 2025/03/13 11:25:05 by anikoyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+extern void	rl_clear_history(void);
+
+void	interactive_loop(char **shell_env)
+{
+	char	*input;
+
+	(void)shell_env;
+	while (true)
+	{
+		input = readline("minishell$ ");
+		if (!input)
+		{
+			move_cursor_up();
+			ft_printf("minishell$ exit\n");
+			break ;
+		}
+		add_history(input);
+	}
+	rl_clear_history();
+}
