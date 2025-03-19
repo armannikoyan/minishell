c2a46d54cb200e909455deb423432ecb9c0a1652
Arman Nikoyan <anikoyan@c2r9s5.42yerevan.am>
2025-03-19 20:42:31 +0400

    changed: functions devided so that norm is not throwing warnings, passing envp as const

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:50:38 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/03/19 12:11:45 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/03/19 20:33:05 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+#include "../includes/utils.h"
 
 extern void	rl_clear_history(void);
 
@@ -21,7 +22,7 @@ int	main(int argc __attribute__((unused)),
 
 	set_term_config(&original_termios);
 	psig_set();
-	interactive_loop(envp);
+	interactive_loop((const char **)envp);
 	restore_terminal_settings(&original_termios);
 	return (EXIT_SUCCESS);
 }
--- src/minishell.c
+++ src/minishell.c
@@ -6,81 +6,100 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 20:17:13 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/03/19 16:47:55 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/03/19 20:33:10 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include "../includes/hash_table.h"
+#include "../includes/utils.h"
 
 extern void	rl_clear_history(void);
 
-static void	ht_insert_env(t_hash_table *ht, char **envp)
+static char	*extract_key(const char *env_entry)
 {
-	char	*str;
-	int		i;
-	int		j;
+	char	*key;
+	char	*delimiter_pos;
+	int		key_length;
 
-	i = 0;
-	j = 0;
-	while (envp[i])
-	{
-		j = ft_strchr(envp[i], '=') - envp[i];
-		str = (char *)malloc(sizeof(char) * (j + 1));
-		if (!str)
-		{
-			perror("malloc");
-			exit(EXIT_FAILURE);
-		}
-		ft_strlcpy(str, envp[i], j + 1);
-		ht_insert(ht, str, &envp[i][j + 1]);
-		free(str);
-		i++;
-	}
-	char *shlvl = ht_get(ht, "SHLVL");
-	if (shlvl)
-		str = ft_itoa(ft_atoi(ht_get(ht, "SHLVL")) + 1);
-	else
-		str = ft_itoa(1);
-	if (!str)
-	{
-		print_error("minishell: Failed to duplicate environment.\n");
-		exit(EXIT_FAILURE);
-	}
-	ht_insert(ht, "SHLVL", str);
-	free(str);
-	str = (char *)malloc(sizeof(char) * PATH_MAX);
-	if (!str)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
-	ht_insert(ht, "PWD", getcwd(str, PATH_MAX));
-	free(str);
-	i = 0;
-	while (i < ht->size)
-	{
-		t_entry *entry = ht->buckets[i];
-		while (entry)
-		{
-			ft_printf("%s=\"%s\"\n", entry->key, entry->value);
-			entry = entry->next;
-		}
-		++i;
-	}
+	if (!env_entry)
+		return (NULL);
+	delimiter_pos = ft_strchr(env_entry, '=');
+	key_length = delimiter_pos - env_entry;
+	key = (char *)malloc(sizeof(char) * (key_length + 1));
+	if (!key)
+		print_error("malloc", EXIT_FAILURE);
+	ft_strlcpy(key, env_entry, key_length + 1);
+	return (key);
 }
 
-void	interactive_loop(char	**envp)
+static void	update_shlvl(t_hash_table *ht)
+{
+	char	*shlvl;
+	char	*new_shlvl;
+
+	if (!ht)
+		return ;
+	shlvl = ht_get(ht, "SHLVL");
+	if (shlvl)
+		new_shlvl = ft_itoa(ft_atoi(ht_get(ht, "SHLVL")) + 1);
+	else
+		new_shlvl = ft_itoa(1);
+	if (!new_shlvl)
+		print_error("malloc", EXIT_FAILURE);
+	ht_insert(ht, "SHLVL", new_shlvl);
+	free(new_shlvl);
+}
+
+static void	update_pwd(t_hash_table *ht)
+{
+	char	*cwd;
+
+	if (!ht)
+		return ;
+	cwd = (char *)malloc(sizeof(char) * PATH_MAX);
+	if (!cwd)
+		print_error("malloc", EXIT_FAILURE);
+	if (getcwd(cwd, PATH_MAX))
+		ht_insert(ht, "PWD", cwd);
+	else
+	{
+		free(cwd);
+		print_error("malloc", EXIT_FAILURE);
+	}
+	free(cwd);
+}
+
+static void	ht_insert_env(t_hash_table *ht, const char **envp)
+{
+	char	*key;
+	int		i;
+
+	if (!ht)
+		return ;
+	i = 0;
+	while (envp[i])
+	{
+		key = extract_key(envp[i]);
+		if (!key)
+			print_error("malloc", EXIT_FAILURE);
+		ht_insert(ht, key, ft_strchr(envp[i], '=') + 1);
+		free(key);
+		++i;
+	}
+	ht_insert(ht, "_", "]");
+	update_pwd(ht);
+	update_shlvl(ht);
+}
+
+void	interactive_loop(const char **envp)
 {
 	t_hash_table	*ht;
 	char			*input;
 
 	ht = ht_create();
 	if (!ht)
-	{
-		print_error("minishell: Failed to duplicate environment.\n");
-		exit(EXIT_FAILURE);
-	}
+		print_error("malloc", EXIT_FAILURE);
 	ht_insert_env(ht, envp);
 	while (true)
 	{
@@ -88,7 +107,7 @@ void	interactive_loop(char	**envp)
 		if (!input)
 		{
 			move_cursor_up();
-			ft_printf("minishell$ exit\n");
+			tputs("minishell$ exit\n", 1, ft_putchar);
 			break ;
 		}
 		add_history(input);
