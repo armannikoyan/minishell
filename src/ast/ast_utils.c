9b6c5bf4658e63809ea9536f3912a4bc6d0d9cb2
Arman Nikoyan <armannikoyan04@icloud.com>
2026-01-03 21:33:49 +0400

    fix: ast node fields are NULL by default.

--- src/ast/ast_utils.c
+++ src/ast/ast_utils.c
@@ -22,7 +22,10 @@ t_ast_node	*create_cmd_node(t_node_type type, char **argv)
 	t_ast_node	*node;
 
 	node = create_node(type);
-	node->u_data.cmd.argv = argv;
+	if (argv)
+		node->u_data.cmd.argv = argv;
+	else
+		node->u_data.cmd.argv = NULL;
 	return (node);
 }
 
@@ -31,6 +34,8 @@ t_ast_node	*create_binary_node(t_node_type type)
 	t_ast_node	*node;
 
 	node = create_node(type);
+	node->u_data.binary.left = NULL;
+	node->u_data.binary.right = NULL;
 	return (node);
 }
 
@@ -39,7 +44,10 @@ t_ast_node	*create_redir_node(t_node_type type, char *filename, int fd)
 	t_ast_node	*node;
 
 	node = create_node(type);
-	node->u_data.redir.filename = filename;
+	if (node->u_data.redir.filename)
+		node->u_data.redir.filename = filename;
+	else
+		node->u_data.redir.filename = NULL;
 	node->u_data.redir.fd = fd;
 	return (node);
 }
