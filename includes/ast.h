7b4a8a6858ccc613b8eb812a61347c841368ba1e
Good-rice-soup <varnachevlev@gmail.com>
2026-01-03 23:34:57 +0400

    feat: added support for binary and command nodes

--- includes/ast.h
+++ includes/ast.h
@@ -3,53 +3,57 @@
 
 # include <stdbool.h>
 
-typedef enum node_type
-{
-	NODE_COMMAND,
-	NODE_PIPE,
-	NODE_AND,
-	NODE_OR,
-	NODE_REDIRECT_IN,
-	NODE_REDIRECT_OUT,
-	NODE_REDIRECT_APPEND,
-	NODE_HEREDOC,
-}	t_node_type;
+typedef enum node_type {
+    NODE_COMMAND,
+    NODE_PIPE,
+    NODE_AND,
+    NODE_OR,
+    NODE_REDIRECT_IN,
+    NODE_REDIRECT_OUT,
+    NODE_REDIRECT_APPEND,
+    NODE_HEREDOC,
+} t_node_type;
 
-struct	s_ast_node;
+typedef enum node_type_abstraction {
+    COMMAND_NODE,
+    BINARY_NODE,
+    REDIRECTION_NODE,
+    UNDEFINED_NODE,
+} t_node_type_abstraction;
 
-struct	s_command
-{
-	char	**argv;
+struct s_ast_node;
+
+struct s_command {
+    char **argv;
 };
 
-struct	s_binary_op
-{
-	struct s_ast_node	*left;
-	struct s_ast_node	*right;
+struct s_binary_op {
+    struct s_ast_node *left;
+    struct s_ast_node *right;
 };
 
-struct	s_redirection
-{
-	struct s_ast_node	*child;
-	char				*filename;
-	int					fd;
+struct s_redirection {
+    struct s_ast_node *child;
+    char *filename;
+    int fd;
 };
 
-typedef struct s_ast_node
-{
-	t_node_type	type;
-	union
-	{
-		struct s_command		cmd;
-		struct s_binary_op		binary;
-		struct s_redirection	redir;
-	}	u_data;
-}	t_ast_node;
+typedef struct s_ast_node {
+    t_node_type type;
 
-t_ast_node	*create_cmd_node(t_node_type type, char **argv);
-t_ast_node	*create_binary_node(t_node_type type);
-t_ast_node	*create_redir_node(t_node_type type, char *filename);
+    union {
+        struct s_command cmd;
+        struct s_binary_op binary;
+        struct s_redirection redir;
+    } u_data;
+} t_ast_node;
 
-t_ast_node	*ast_build(t_ast_node *new_node, t_ast_node *head_node);
+t_ast_node *create_cmd_node(t_node_type type, char **argv);
+
+t_ast_node *create_binary_node(t_node_type type);
+
+t_ast_node *create_redir_node(t_node_type type, char *filename);
+
+t_ast_node *ast_build(t_ast_node *new_node, t_ast_node *head_node);
 
 #endif
--- src/ast/ast.c
+++ src/ast/ast.c
@@ -22,35 +22,102 @@ const char *node_type_to_str(t_node_type type) {
     }
 }
 
-// t_ast_node *upd_tree(t_ast_node *new_node, t_ast_node *head_node) {
-//     t_node_type new_node_type;
-//     t_node_type head_node_type;
-//
-//     new_node_type = new_node->type;
-//     head_node_type = head_node->type;
-//
-//     if (head_node_type == NODE_COMMAND && new_node_type == NODE_COMMAND) {
-//         print_error("Abstract tree syntax error occurred\n");
-//         return NULL;
-//     }
-//
-// }
+t_node_type_abstraction get_node_type_abstraction(t_node_type type) {
+    if (type == NODE_COMMAND)
+        return COMMAND_NODE;
+    if (type == NODE_PIPE)
+        return BINARY_NODE;
+    if (type == NODE_AND)
+        return BINARY_NODE;
+    if (type == NODE_OR)
+        return BINARY_NODE;
+    if (type == NODE_REDIRECT_IN)
+        return REDIRECTION_NODE;
+    if (type == NODE_REDIRECT_OUT)
+        return REDIRECTION_NODE;
+    if (type == NODE_REDIRECT_APPEND)
+        return REDIRECTION_NODE;
+    if (type == NODE_HEREDOC)
+        return REDIRECTION_NODE;
+    return UNDEFINED_NODE;
+}
+
+t_ast_node *ast_tree_error() {
+    print_error("Abstract tree syntax error occurred: impossible node combination\n");
+    return NULL;
+}
+
+int is_binary_node_full(t_ast_node *node) {
+    int right_full;
+    int left_full;
+
+    right_full = node->u_data.binary.right != NULL;
+    left_full = node->u_data.binary.left != NULL;
+    return right_full && left_full;
+}
+
+int is_binary_node_filled_right(t_ast_node *node) {
+    int right_correct;
+    int left_correct;
+
+    right_correct = node->u_data.binary.right != NULL;
+    left_correct = node->u_data.binary.left == NULL;
+    return right_correct && left_correct;
+}
+
+
+t_ast_node *upd_tree(t_ast_node *new_node, t_ast_node *head_node) {
+    t_node_type_abstraction new_node_type;
+    t_node_type_abstraction head_node_type;
+
+    if (head_node == NULL)
+        return new_node;
+    new_node_type = get_node_type_abstraction(new_node->type);
+    head_node_type = get_node_type_abstraction(head_node->type);
+
+    if (head_node_type == COMMAND_NODE) {
+        if (new_node_type == BINARY_NODE) {
+            new_node->u_data.binary.left = head_node;
+            return new_node;
+        }
+    }
+    if (head_node_type == BINARY_NODE) {
+        if (new_node_type == COMMAND_NODE) {
+            if (is_binary_node_filled_right(head_node)) {
+                head_node->u_data.binary.right = new_node;
+                return head_node;
+            }
+        }
+        if (new_node_type == BINARY_NODE) {
+            if (is_binary_node_full(head_node)) {
+                new_node->u_data.binary.left = head_node;
+                return new_node;
+            }
+        }
+    }
+    return ast_tree_error();
+}
 
 // Takes a head node of abstract tree and a new node, and returns an updated ast tree.
 // If a syntaxis error occurred while updating tree, returns NULL
 t_ast_node *ast_build(t_ast_node *new_node, t_ast_node *head_node) {
-    if (new_node->type == NODE_COMMAND) {
-        printf("New node - NODE_COMMAND: ");
+    t_node_type_abstraction node_type_abstraction;
+
+    node_type_abstraction = get_node_type_abstraction(new_node->type);
+    if (node_type_abstraction == COMMAND_NODE) {
+        printf("New node - COMMAND_NODE: ");
         for (size_t i = 0; new_node->u_data.cmd.argv[i]; ++i)
             printf("%s ", new_node->u_data.cmd.argv[i]);
         printf("\n");
-    } else if (new_node->type == NODE_AND || new_node->type == NODE_OR || new_node->type == NODE_PIPE) {
-        printf("New node - NODE_BINARY[%s]\n", node_type_to_str(new_node->type));
-    } else {
-        printf("New node - NODE_REDIR[%s]\n", node_type_to_str(new_node->type));
+    } else if (node_type_abstraction == BINARY_NODE) {
+        printf("New node - BINARY_NODE[%s]\n", node_type_to_str(new_node->type));
+    } else if (node_type_abstraction == REDIRECTION_NODE) {
+        printf("New node - REDIRECTION_NODE[%s]\n", node_type_to_str(new_node->type));
         printf("Redirection path: %s\n", new_node->u_data.redir.filename);
+    } else {
+        printf("New node - UNKNOWN_NODE\n");
     }
 
 
-    return (head_node);
+    return upd_tree(new_node, head_node);
 }
