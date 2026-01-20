ac6386b9d3ff0bde9561b51b0af4ed0527f7e2d3
Good-rice-soup <varnachevlev@gmail.com>
2026-01-20 15:10:31 +0400

    fix: incorrect printing of exported environment variables

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -3,27 +3,33 @@
 #include "hash_table.h"
 #include "utils.h"
 
+static void inner_circle(t_entry *entry) {
+    while (entry) {
+        if (entry->is_local == false) {
+            if (entry->val == NULL)
+                printf("%s\n", entry->key);
+            else if (entry->val[0] == '\0')
+                printf("%s=""\n", entry->key);
+            else
+                printf("%s=%s\n", entry->key, entry->val);
+        }
+        entry = entry->next;
+    }
+}
+
 // Prints all environment variables and finishes with status 0.
 // In case of no environment variables prints noting.
 // If any arguments passed prints an error and finishes with status 2.
 int ft_env(int argc, char **argv, t_hash_table *ht) {
-    t_entry *entry;
     int i;
 
     (void) argv;
-    if (argc > 1) {
-        //TODO: make normal error
-        print_error("env: too many arguments\n", true);
-        return 2;
-    }
+    //TODO: make normal error
+    if (argc > 1)
+        return (print_error("env: too many arguments\n", true), 2);
     i = 0;
     while (i < ht->size) {
-        entry = ht->buckets[i];
-        while (entry) {
-            if (entry->is_local == false)
-                printf("%s=%s\n", entry->key, entry->val);
-            entry = entry->next;
-        }
+        inner_circle(ht->buckets[i]);
         i++;
     }
     return (0);
