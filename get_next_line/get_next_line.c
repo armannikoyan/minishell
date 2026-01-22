5447b69e2e06cc4d9bd45c72ca708e23387f279e
Arman Nikoyan <armannikoyan04@icloud.com>
2026-01-22 23:19:13 +0400

    feat: gnl for reading from not tty mode

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,100 @@
+#include "get_next_line.h"
+#include "../libs/libft/libft.h"
+
+char	*ft_read_fd(int fd, char *buffer)
+{
+	char	*tmp;
+	char	*ptr_buffer;
+	int		bytes_read;
+
+	tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!tmp)
+		return (NULL);
+	bytes_read = 1;
+	while ((!buffer || !ft_strchr(buffer, '\n')) && bytes_read)
+	{
+		ptr_buffer = buffer;
+		bytes_read = read(fd, tmp, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(tmp);
+			free(buffer);
+			return (NULL);
+		}
+		tmp[bytes_read] = '\0';
+		buffer = ft_strjoin(buffer, tmp);
+		free(ptr_buffer);
+	}
+	free(tmp);
+	return (buffer);
+}
+
+char	*ft_get_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_remove_line(char *buffer)
+{
+	char	*new_buffer;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	new_buffer = malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
+	if (!new_buffer)
+		return (NULL);
+	j = 0;
+	while (buffer[i])
+		new_buffer[j++] = buffer[i++];
+	new_buffer[j] = '\0';
+	free(buffer);
+	return (new_buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = ft_read_fd(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = ft_get_line(buffer);
+	buffer = ft_remove_line(buffer);
+	return (line);
+}
