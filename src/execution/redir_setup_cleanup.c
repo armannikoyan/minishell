0711419b471c51544f0b5c8e62d1ec59e726d6bb
Arman Nikoyan <armannikoyan04@icloud.com>
2026-01-28 20:20:52 +0400

    fix: linux header.

--- src/execution/redir_setup_cleanup.c
+++ src/execution/redir_setup_cleanup.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include <sys/fcntl.h>
-#include <sys/syslimits.h>
+#include <limits.h>
 
 #include "ast.h"
 #include "execution.h"
